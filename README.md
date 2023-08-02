46f937b41227e516681d348fd38147dd8610ae4a
whipowill <whipowill@protonmail.com>
2023-08-01 19:14:56 -0600

    Init.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Nerf Herder
+
+An AzerothCore module for nerfing NPCs throughout the game to the appropriate level, such as guards and even faction leaders.
+
+Primarily, this mod is intended for private servers limited to Vanilla or TBC, who want the towns folk to be limited to levels 60 or 70.
+
+Secondarily, this mod is designed to detect [AutoBalance](https://github.com/azerothcore/mod-autobalance.git) and allow for invasion of faction cities and assassination of faction leaders.
+
+Tertiarily, I might eventually add on some all around nerf/buff features for all classes.
+
+THIS IS A WORK IN PROGRESS AND IS NOT READY FOR USE
+
+## Methodology
+
+This module uses [55Honey's ZoneDebuff](https://github.com/55Honey/Acore_ZoneDebuff/blob/master/zoneDebuff.lua) technique to apply a debuff to all townsfolk.
+
+## Credits
+
+- ChatGPT
+- 55Honey
+
+## External Links
+
+- [AzerothCore](https://github.com/azerothcore/azerothcore-wotlk)
+- [ZoneDebuff](https://github.com/55Honey/Acore_ZoneDebuff/blob/master/zoneDebuff.lua)
---
+++ src/ModNerfHerder.cpp
@@ -0,0 +1,56 @@
+#include "ModNerfHerder.h"
+#include "ScriptMgr.h"
+#include "CreatureData.h"
+#include "Config.h"
+
+class NerfHerder : public WorldScript
+{
+public:
+    NerfHerder() : WorldScript("NerfHerder") {}
+
+    void OnCreatureLoad(Creature* creature) override
+    {
+        if (creature->IsPlayer())
+        {
+            return;
+        }
+
+        uint32_t is_enabled = sConfigMgr->GetOption<int>("NerfHerder.Enable", 0);
+        uint32_t max_level = sConfigMgr->GetOption<int>("MaxPlayerLevel", 60); // <-- from worldserver.conf
+
+        if (is_enabled)
+        {
+            if (creature->GetLevel() > max_level)
+            {
+                uint32_t new_level = creature->IsElite() ? max_level : max_level - 5;
+                int32_t multiplier = -100 + ((new_level / creature->GetLevel()) * 100); // calc negative multiplier
+
+                // set new level
+                creature->SetLevel(new_level);
+
+                // nerf auras
+                uint32_t HpAura= 89501;
+                uint32_t DamageDoneTakenAura = 89502;
+                uint32_t BaseStatAPAura = 89503;
+                uint32_t RageFromDamageAura = 89504;
+                uint32_t AbsorbAura = 89505;
+                uint32_t HealingDoneAura = 89506;
+                uint32_t PhysicalDamageTakenAura = 89507;
+
+                // nerf their abilities proportionately
+                creature->CastCustomSpell(player, HpAura, &multiplier, NULL, NULL, true, NULL, NULL, creature->GetGUID());
+                creature->CastCustomSpell(player, DamageDoneTakenAura, NULL, &multiplier, NULL, true, NULL, NULL, creature->GetGUID());
+                creature->CastCustomSpell(player, BaseStatAPAura, &multiplier, &multiplier, &multiplier, true, NULL, NULL, creature->GetGUID());
+                //creature->CastCustomSpell(player, RageFromDamageAura, &RageFromDamageModifier, NULL, NULL, true, NULL, NULL, creature->GetGUID());
+                creature->CastCustomSpell(player, AbsorbAura, &multiplier, NULL, NULL, true, NULL, NULL, creature->GetGUID());
+                creature->CastCustomSpell(player, HealingDoneAura, &multiplier, NULL, NULL, true, NULL, NULL, creature->GetGUID());
+                //creature->CastCustomSpell(player, PhysicalDamageTakenAura, &PhysicalDamageTakenModifier, NULL, NULL, true, NULL, NULL, creature->GetGUID());
+            }
+        }
+    }
+};
+
+void AddNerfHerderScripts()
+{
+    new NerfHerder();
+}
