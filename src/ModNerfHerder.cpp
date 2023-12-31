11127f5d0ef02000c1310b775c0a86ab386a8d95
whipowill <whipowill@protonmail.com>
2023-12-30 20:29:15 -0700

    Updates.

--- src/ModNerfHerder.cpp
+++ src/ModNerfHerder.cpp
@@ -293,7 +293,7 @@ public:
     static uint64_t GetTime()
     {
         // number of seconds
-        return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     }
 };
 
